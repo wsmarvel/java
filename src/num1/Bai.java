178a4e12a7ead34ee7aa2a1243863cb1e47c8447
bye <bye@bye-PC>
2018-04-17 10:27:34 +0800

    no1-2

--- src/num1/Bai.java
+++ src/num1/Bai.java
@@ -18,12 +18,13 @@ public class Bai {
 
 public static void main(String args[]) {
 	Bai bai = new Bai("bai");
-	
+	 /* 创建对象 */
 	bai.setAge(2);
-	
+	/* 通过方法来设定age */
 	bai.getAge();
-	
+	/* 调用另一个方法获取age */
 	System.out.println("成员变量为"+ bai.myAge);
+	/*也可以像上面这样访问成员变量 */
 }
 
 }
