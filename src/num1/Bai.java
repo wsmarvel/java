d545a4a5ff08f531aae3450b6f8f0ed4f958f3a9
bye <bye@bye-PC>
2018-04-17 10:23:55 +0800

    no1-2

--- src/num1/Bai.java
+++ src/num1/Bai.java
@@ -1,5 +1,29 @@
 package num1;
 
 public class Bai {
+	int myAge;
+	public Bai(String name) {
+		System.out.println("my name is " + name);
+		
+	}
+
+	public void setAge(int age) {
+		myAge = age;
+	}
+
+	public int getAge(){
+		System.out.println("年龄是 "+ myAge);
+		return myAge;
+	}
+
+public static void main(String args[]) {
+	Bai bai = new Bai("bai");
+	
+	bai.setAge(2);
+	
+	bai.getAge();
+	
+	System.out.println("成员变量为"+ bai.myAge);
+}
 
 }
